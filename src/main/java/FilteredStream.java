1bc9886efd034a3b87233481bf1fb9dc84a7627c
Hrishikesh Deshmukh <hdeshmukh@gohealth.com>
2021-06-30 15:45:34 -0500

    Refactor filter stream search

--- src/main/java/FilteredStream.java
+++ src/main/java/FilteredStream.java
@@ -1,2 +1,89 @@
-package PACKAGE_NAME;public class FilterStream {
+import twitter4j.*;
+
+/**
+ * Filtered Stream: Get all the Tweets in real-time that match the search criteria you've set.
+ */
+public class FilteredStream {
+
+    private final Twitter twitter;
+
+    public FilteredStream(Twitter twitter) {
+        this.twitter = twitter;
+    }
+
+    /**
+     * Finds and prints real-time tweets based on the searchTerm passed
+     * By default returns 5 tweets
+     * Query object allows searching for certain strings while Status object represents an individual tweet
+     * Reference: https://twitter4j.org/en/code-examples.html
+     *
+     * @param searchTerm The phrase that will be searched
+     */
+    public void search(String searchTerm) {
+        try {
+            // Each QueryResult will get around 15 tweets, unless otherwise assigned
+            Query query = new Query(searchTerm);
+            QueryResult result;
+            result = twitter.search(query);
+            int tweetNumber = 1;
+            for (Status status : result.getTweets()) {
+                if (tweetNumber <= 5) {
+                    printTweet(status, 1, tweetNumber);
+                    tweetNumber++;
+                }
+            }
+        } catch (TwitterException twe) {
+            twe.printStackTrace();
+            System.out.println("Could not find tweets due to a TwitterException error: " + twe.getErrorMessage());
+        }
+    }
+
+    /**
+     * Finds and prints real-time tweets based on the searchTerm passed
+     * Returns amount of tweets specified (numOfQueryResults * numOfTweets)
+     * Query object allows searching for certain strings while Status object represents an individual tweet
+     * Reference: https://twitter4j.org/en/code-examples.html
+     *
+     * @param searchTerm        The phrase that will be searched
+     * @param numOfQueryResults Amount of Query Result objects to search; each contains a max of 15 tweets
+     * @param numOfTweets       Amount of tweets to search in each Query Result; does not exceed 15
+     */
+    public void search(String searchTerm, int numOfQueryResults, int numOfTweets) {
+        try {
+            if (numOfQueryResults <= 0 || numOfTweets <= 0) {
+                return;
+            }
+            Query query = new Query(searchTerm);
+            QueryResult result;
+            int queryResultNum = 1;
+
+            do {
+                result = twitter.search(query);
+                int tweetNumber = 1;
+                for (Status status : result.getTweets()) {
+                    if (tweetNumber <= numOfTweets) {
+                        printTweet(status, queryResultNum, tweetNumber);
+                        tweetNumber++;
+                    }
+                }
+                queryResultNum++;
+            } while ((query = result.nextQuery()) != null && queryResultNum <= numOfQueryResults);
+        } catch (TwitterException twe) {
+            twe.printStackTrace();
+            System.out.println("Could not find tweets due to a TwitterException error: " + twe.getErrorMessage());
+        }
+    }
+
+    /**
+     * Elegantly formats and prints tweets found using either search method
+     *
+     * @param status         Status object representing an individual tweet
+     * @param queryResultNum Current query result in search
+     * @param tweetNumber    Current tweet in search
+     */
+    private static void printTweet(Status status, int queryResultNum, int tweetNumber) {
+        System.out.println("Query Result Number #" + queryResultNum + ", Tweet Number #" + tweetNumber);
+        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+        System.out.println("*************************************************************");
+    }
 }
--- src/main/java/TwitterApp.java
+++ src/main/java/TwitterApp.java
@@ -1,8 +1,6 @@
 import twitter4j.*;
-import twitter4j.conf.ConfigurationBuilder;
 
-// Filtered Stream: Get all the Tweets in real-time that match the search criteria you've set.
-public class FilteredStreamTwitter4J {
+public class TwitterApp {
 
     public static void main(String args[]) {
         PropertyReader propertyReader = new PropertyReader();
@@ -13,32 +11,8 @@ public class FilteredStreamTwitter4J {
         TwitterFactory twitterFactory = credentialConfig.createTwitterFactory();
         Twitter twitter = twitterFactory.getInstance();
 
-        // Reference this site: https://twitter4j.org/en/code-examples.html
-        try {
-            // Query object allows you to search for certain strings
-            // Each QueryResult will get around 15 tweets, unless otherwise assigned
-            Query query = new Query("GoHealth");
-            QueryResult result;
-            int queryResultNum = 1;
-            // TODO: Catch or manage SocketTimeoutException that occasionally occurs
-            do {
-                result = twitter.search(query);
-                int tweetNumber = 1;
-                // Each Status object represents an individual tweet
-                for (Status status : result.getTweets()) {
-                    if (tweetNumber <= 20) {
-                        System.out.println("Query Result Number #" + queryResultNum + ", Tweet Number #" + tweetNumber);
-                        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-                        System.out.println("*************************************************************");
-                        tweetNumber++;
-                    }
-                }
-                queryResultNum++;
-            } while ((query = result.nextQuery()) != null && queryResultNum < 2);
-        } catch (TwitterException twe) {
-            twe.printStackTrace();
-            System.out.println("Could not find tweets due to a TwitterException error: " + twe.getErrorMessage());
-        }
+        FilteredStream filteredStream = new FilteredStream(twitter);
+        filteredStream.search("Lebron", 1, 1);
 
     }
 }
