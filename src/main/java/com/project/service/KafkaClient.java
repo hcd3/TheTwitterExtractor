30b39fe230de2c2994634060925db4c31db18c46
Hrishikesh Deshmukh <hdeshmukh@gohealth.com>
2021-07-01 12:24:23 -0500

    Modify KafkaClient class

--- src/main/java/com/project/service/KafkaClient.java
+++ src/main/java/com/project/service/KafkaClient.java
@@ -2,11 +2,13 @@ package com.project.service;
 
 import com.project.model.Tweet;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.List;
 
 public class KafkaClient {
 
-    @Value("${kafka.topic.twitter-extractor-topic}")
     private final String topic;
     private final KafkaTemplate<String, Tweet> kafkaTemplate;
 
@@ -16,18 +18,31 @@ public class KafkaClient {
      * @param topic         Kafka topic tweets will be sent to
      * @param kafkaTemplate Kafka template to wrap around
      */
-    public KafkaClient(String topic, KafkaTemplate<String, Tweet> kafkaTemplate) {
+    public KafkaClient(@Value("${kafka.topic.twitter-extractor-topic}") String topic,
+                       KafkaTemplate<String, Tweet> kafkaTemplate) {
         this.topic = topic;
         this.kafkaTemplate = kafkaTemplate;
     }
 
     /**
      * Acts as a wrapper function for KafkaTemplate's send() function
-     * Sends Tweet to Kafka topic
+     * Sends (produces) tweets to Kafka topic
      *
-     * @param tweet The Tweet object to send
+     * @param listOfTweets The list of Tweet objects to individually send
      */
-    public void sendToTopic(Tweet tweet) {
-        kafkaTemplate.send(topic, tweet);
+    public void sendToTopic(List<Tweet> listOfTweets) {
+        for (Tweet tweet : listOfTweets) {
+            kafkaTemplate.send(topic, tweet);
+        }
+    }
+
+    /**
+     * Consumes Tweet from Kafka topic
+     *
+     * @param tweet The consumed tweet
+     */
+    @KafkaListener(topics = "${kafka.topic.twitter-extractor-topic}")
+    protected void consumeFromTopic(Tweet tweet){
+
     }
 }
